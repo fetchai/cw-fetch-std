0e55e97bf63acf2c2c31fe300902d7055b3c320c
Jiri <jiri.vestfal@fetch.ai>
2024-04-15 16:12:01 +0200

    Additional tests helpers

--- src/testing/helpers.rs
+++ src/testing/helpers.rs
@@ -1,9 +1,9 @@
 use cosmwasm_std::testing::{mock_dependencies, MockApi, MockQuerier, MockStorage};
-use cosmwasm_std::ContractResult as StdContractResult;
 use cosmwasm_std::{
     to_json_binary, Addr, ContractInfoResponse, Empty, OwnedDeps, SystemError, SystemResult,
     WasmQuery,
 };
+use cosmwasm_std::{BankMsg, Coin, ContractResult as StdContractResult, Response, SubMsg, Uint128};
 
 pub fn deps_with_creator(
     creator: Addr,
@@ -30,4 +30,24 @@ pub fn deps_with_creator(
     });
     deps.querier = querier;
     deps
+}
+
+pub fn assert_err<T, E: std::fmt::Debug>(result: Result<T, E>, error: &E) {
+    // Check if result contains specific error
+    match result {
+        Ok(_) => panic!("Expected Err, got Ok"),
+        Err(res_error) => assert_eq!(format!("{:?}", res_error), format!("{:?}", error)),
+    }
+}
+
+pub fn assert_transfer(res: &Response, address: &Addr, amount: &u128, denom: &str) {
+    let send_msg = SubMsg::new(BankMsg::Send {
+        to_address: address.to_string(),
+        amount: vec![Coin {
+            denom: denom.to_string(),
+            amount: Uint128::new(*amount),
+        }],
+    });
+
+    assert_eq!(res.messages[0], send_msg);
 }
