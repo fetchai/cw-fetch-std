2fad83103d48c0c2b31985aed10630173a051630
Jiri <jiri.vestfal@fetch.ai>
2024-04-17 17:26:37 +0200

    Refactor

--- src/crypto/secp256k1.rs
+++ src/crypto/secp256k1.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::{StdError, StdResult};
 
 pub type CompressedPubkey = [u8; 33];
 
-fn compress_pubkey(uncompressed_pubkey_bytes: &[u8]) -> Result<CompressedPubkey, StdError> {
+fn compress_pubkey(uncompressed_pubkey_bytes: &[u8]) -> CompressedPubkey {
     // The first byte is the prefix, followed by 32 bytes for X and 32 bytes for Y
     let x_bytes = &uncompressed_pubkey_bytes[0..32];
     let y_bytes = &uncompressed_pubkey_bytes[32..64];
@@ -11,28 +11,26 @@ fn compress_pubkey(uncompressed_pubkey_bytes: &[u8]) -> Result<CompressedPubkey,
     // Y's last byte's least significant bit determines its evenness or oddness
     let prefix_byte = if (y_bytes[31] & 1) == 0 { 0x02 } else { 0x03 };
 
-    // Create the compressed public key
-    let mut compressed_pubkey: Vec<u8> = Vec::with_capacity(33);
-    compressed_pubkey.push(prefix_byte);
-    compressed_pubkey.extend_from_slice(x_bytes);
+    // Create the compressed public key array
+    let mut compressed_pubkey = [0u8; 33];
+    compressed_pubkey[0] = prefix_byte;
+    compressed_pubkey[1..].copy_from_slice(x_bytes);
 
-    Ok(<[u8; 33]>::try_from(compressed_pubkey).unwrap())
+    compressed_pubkey
 }
 
 pub fn to_compressed_key(pubkey: &[u8]) -> StdResult<CompressedPubkey> {
     match pubkey.len() {
-        33 => {
-            // Compressed pubkey
-            Ok(pubkey.try_into().unwrap())
-        }
-        64 => {
-            // Uncompressed without checksum
-            compress_pubkey(pubkey)
-        }
-        65 => {
-            // Uncompressed with checksum
-            compress_pubkey(&pubkey[1..])
-        }
+        // Compressed pubkey
+        33 => pubkey
+            .try_into()
+            .map_err(|_| pubkey_error(&"Conversion error")),
+
+        // Uncompressed without checksum
+        64 => Ok(compress_pubkey(pubkey)),
+
+        // Uncompressed with checksum
+        65 => Ok(compress_pubkey(&pubkey[1..])),
         _ => Err(pubkey_error(&"Wrong len")),
     }
 }
