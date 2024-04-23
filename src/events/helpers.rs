cde4538538ab5b9d54a3e09ab9b7a962ec1ae466
Jiri <jiri.vestfal@fetch.ai>
2024-04-23 12:51:15 +0200

    Rename

--- src/events/helpers.rs
+++ src/events/helpers.rs
@@ -1,3 +1,3 @@
-pub fn to_string_or_none<T: ToString>(value: Option<T>) -> String {
+pub fn option_to_string<T: ToString>(value: Option<T>) -> String {
     value.map_or_else(|| "none".to_string(), |val| val.to_string())
 }
