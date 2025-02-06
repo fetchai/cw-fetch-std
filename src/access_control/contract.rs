18d51bdcb810ed423f81a9f62bd1f17f1e42213e
Jiri <jiri.vestfal@fetch.ai>
2025-02-06 17:58:36 +0100

    queries

--- src/access_control/contract.rs
+++ src/access_control/contract.rs
@@ -1,44 +1,16 @@
 use crate::access_control::AccessControl;
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-use serde::Deserialize;
 
 #[cw_serde]
-pub struct QueryAllAddressesWithRoleResponse {
-    pub addresses: Vec<Addr>,
+pub struct QueryHasRoleResponse {
+    pub has_role: bool,
 }
 
-#[cw_serde]
-pub struct QueryRolesResponse {
-    pub roles: Vec<String>,
-}
-
-pub fn query_all_addresses_with_role<T: serde::Serialize + for<'a> Deserialize<'a> + Clone>(
-    deps: Deps,
-    role: String,
-) -> StdResult<QueryAllAddressesWithRoleResponse> {
-    let addresses_with_role: StdResult<Vec<Addr>> =
-        AccessControl::range_all_addresses_with_role(deps.storage, &role)?.collect();
-
-    Ok(QueryAllAddressesWithRoleResponse {
-        addresses: addresses_with_role?,
+pub fn query_has_role(deps: Deps, addr: Addr, role: String) -> StdResult<QueryHasRoleResponse> {
+    Ok(QueryHasRoleResponse {
+        has_role: AccessControl::has_role(deps.storage, &role, &addr),
     })
-}
-
-pub fn query_roles(deps: Deps, addr: Addr) -> StdResult<QueryRolesResponse> {
-    let res_all_roles: StdResult<Vec<String>> =
-        AccessControl::range_all_roles(deps.storage).collect();
-    let all_roles = res_all_roles?;
-
-    let mut addr_roles: Vec<String> = Vec::new();
-
-    for role in all_roles {
-        if AccessControl::has_role(deps.storage, &role, &addr) {
-            addr_roles.push(role.to_string());
-        }
-    }
-
-    Ok(QueryRolesResponse { roles: addr_roles })
 }
 
 pub fn execute_grant_role_by_admin_role(
