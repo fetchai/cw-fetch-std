bbb4dd59f6b11c8cee1bfdc8f3e66e779b75fcf5
Jiri <jiri.vestfal@fetch.ai>
2025-04-02 09:48:17 +0200

    refactor: Access control messages

--- src/access_control/mod.rs
+++ src/access_control/mod.rs
@@ -2,10 +2,12 @@ mod access_control_impl;
 mod contract;
 pub mod error;
 mod events;
+mod msg;
 mod role;
 mod storage;
 
 pub use access_control_impl::*;
 pub use contract::*;
 pub use events::*;
+pub use msg::*;
 pub use role::*;
